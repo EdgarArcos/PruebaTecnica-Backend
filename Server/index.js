92c1411024952e853c03908b61786443ea5cd05d
Edgar Arcos <edgararcosmartin@gmail.com>
2023-05-22 13:08:18 +0100

    Add routes mongodb controllers env

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,9 @@
+import postsRoutes from "./routes/posts.routes.js";
 import express from "express";
+import { connectDB } from "./db.js";
 const app = express()
+connectDB()
+app.use(postsRoutes)
 
 app.listen(4000)
 console.log("Server is running");
